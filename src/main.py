3aa8172f2c5a0e4de352895dfc0b833475469548
Matt Doyle <mdoyle179@gmail.com>
2018-12-06 22:44:58 -0500

    Moved printPlayers and createPlot into Players class

--- src/main.py
+++ src/main.py
@@ -1,42 +1,12 @@
-import tweepy
 import player as player
 import game as game
 import fantasyExpert as fantasyExpert
-import matplotlib.pyplot as plt
-from operator import attrgetter
-
-# Creates a scatterplot
-def createPlot(players):
-    sortedList = sorted(players, key=attrgetter('games.points'))
-    x = []
-    y = []
-    for player1 in sortedList:
-        x.append(player1.games.points)
-        y.append(player1.games.numOfTweets)
-    
-    colors = (0,0,0)
-     
-    # Plot
-    plt.scatter(x, y, s=15, c=colors, alpha=0.5)
-    plt.title('Fantasy Football points vs # of Tweets')
-    plt.xlabel('# of Points')
-    plt.ylabel('# of Tweets')
-    plt.show()
     
 # Converts the file to an array of lines
 def getLines(fileName):
     with open(fileName) as f:
         lines = f.readlines()
     return lines
-
-def printPlayers():
-    tableTitle = "{:^20}  {:^20}  {:^10}".format("Player Name", "Points", "Tweets")
-    print(tableTitle)
-
-    sortedList = sorted(players, key=attrgetter('games.points'))
-    for player1 in sortedList:
-        formatted = "{:^20}  {:^20}  {:^10}".format(player1.name, player1.games.points, player1.games.numOfTweets)
-        print(formatted)
 
 # Parses the data and creates the Game and Player objects        
 def parseData(line):
--- src/player.py
+++ src/player.py
@@ -1,5 +1,36 @@
+from operator import attrgetter
+import matplotlib.pyplot as plt
+
+# Represents a football player
 class Player:
     def __init__(self, name, nickname, games):
         self.name = name
         self.games = games
         self.nickname = nickname
+
+    def printPlayers(players):
+        tableTitle = "{:^20}  {:^20}  {:^10}".format("Player Name", "Points", "Tweets")
+        print(tableTitle)
+    
+        sortedList = sorted(players, key=attrgetter('games.points'))
+        for player1 in sortedList:
+            formatted = "{:^20}  {:^20}  {:^10}".format(player1.name, player1.games.points, player1.games.numOfTweets)
+            print(formatted)
+            
+    # Creates a scatterplot
+    def createPlot(players):
+        sortedList = sorted(players, key=attrgetter('games.points'))
+        x = []
+        y = []
+        for player1 in sortedList:
+            x.append(player1.games.points)
+            y.append(player1.games.numOfTweets)
+        
+        colors = (0,0,0)
+         
+        # Plot
+        plt.scatter(x, y, s=15, c=colors, alpha=0.5)
+        plt.title('Fantasy Football points vs # of Tweets')
+        plt.xlabel('# of Points')
+        plt.ylabel('# of Tweets')
+        plt.show()
